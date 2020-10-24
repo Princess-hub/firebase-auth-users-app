2449b372a7c201e6d4ad8856b5f8d54e814fc50e
Princess <plinda.mensah@gmail.com>
2020-10-24 18:02:53 +0000

    App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import Users from './components/Users';
+import AddUserForm from './components/AddUserForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          name: "James k. Mensah",
+          email: "jkm.mensah@gmail.com",
+          gen: 4, 
+          id: "frf",
+        },
+        {
+          name: "James k. Mensah",
+          email: "jkm.mensah@gmail.com",
+          gen: 4, 
+          id: "rfr",
+        },
+        {
+          name: "James k. Mensah",
+          email: "jkm.mensah@gmail.com",
+          gen: 4,
+          id: "frr", 
+        }
+      ]
+    }
+
+  }
+  addNewUser = (user) => {
+    user.id=Math.random().toString()
+    this.setState({
+      users: [...this.state.users, user]
+    })
+  }
+  deleteUser = (id) => {
+    let undeletedUsers = this.state.users.filter(user => user.id !== id);
+    this.setState({
+      users: undeletedUsers
+    })
+  }
+  editUser = (id, updatedUser) => {
+    this.setState({
+      users: this.state.users.map(user => user.id === id ? updatedUser: user)
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <Container fluid style={{marginTop: "2rem"}}>
+          <Row>
+            <Col md="4">
+              <h3>Add Users</h3>
+              <AddUserForm addUser={this.addNewUser} />
+            </Col>
+            <Col>
+            <h3>All Codetrain Users</h3>
+              <Users  usersData={this.state.users} deleteUser={this.deleteUser} edeitUser={this.editUser} />
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
 
 export default App;
+
