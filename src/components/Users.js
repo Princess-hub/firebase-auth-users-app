85be62ef5506f9bec78919cd6b7a71f57b565208
Princess Linda Mensah <plinda.mensah@gmail.com>
2021-02-07 22:48:36 +0000

    changes-firebase

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {compose} from 'redux';
+import {firestoreConnect} from 'react-redux-firebase';
 import { Container, Row } from 'react-bootstrap';
 import User from './User';
 
 const Users = (props) => {
-    console.log(props)
+    let displayInfo;
+    if (props.requesting) {
+        displayInfo = <p>Loading...</p>
+    } else if (props.requested && props.users.length === 0) {
+        displayInfo = <p>No users found</p>
+    } else if (props.requested && props.users.length > 0) {
+        displayInfo = props.users.map((user) => {
+            return(
+                <User userInfo={user} key={user.id} editUser={props.editUser} />
+            );
+        })
+    }
     return (
         <Container>
             <Row>
-                {
-                    props.usersData.map (
-                    (user) => <User
-                    userInfo={user}
-                    key={user.id}
-
-                        
-                    />
-                    )
-                }
+                { displayInfo }
 
             </Row>
         </Container>
@@ -26,7 +30,10 @@ const Users = (props) => {
 
  
 const mapStateToProps = (state) => ({
-    usersData: state.userState.users
+    usersData: state.userState.users,
+    users:state.firestore.ordered.user,
+    requesting: state.firestore.status.requesting.user,
+    requested:state.firestore.status.requested.user,
 })
 
-export default connect(mapStateToProps)(Users);
+export default compose(connect(mapStateToProps), firestoreConnect)(["user"])(Users);
